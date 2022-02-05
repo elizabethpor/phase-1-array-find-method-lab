03502dac3d725dc51c60c9a63787df5c7dcaa1b9
Elizabeth <elizabeth.por@gmail.com>
2022-02-05 10:50:37 -0600

    progress so far, got the record but not the year

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// code your solution here
+// how to write the function with a for of loop:
+// function loopThroughAndFind(array, finder) {
+//     let win = undefined;
+//     for (let item of array) {
+//         if(finder(item) === true) {
+//             win = item.year;
+//             break;
+//         }
+//     }
+//     return win;
+// }
+// function isW(game){
+//     return game.result === 'W';
+// }
+// loopThroughAndFind(record, isW);
+// it returns the year!!!
+
+
+//how to write the function with .find(), but it finds
+//the whole record not just the year:
+// function isW(game){
+//     return game.result === 'W';
+//}
+// letFoundItemYear = record.find(isW);
+//LOG: {year: '2015', result: 'W'}
+
+function superbowlWin(record) {
+    return record.result === 'W';
+}
+let foundRecordYear = record.find(superbowlWin);
+console.log(foundRecordYear);
+
+
+
+
+
+
